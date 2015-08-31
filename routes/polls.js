fbeac9cb6b751200d7fd7ce5e544698ec5272e5a
Cat <cat.r.mocha@gmail.com>
2015-08-31 10:49:33 -0700

    added mongolab details

--- routes/polls.js
+++ routes/polls.js
@@ -12,7 +12,7 @@ router.post('/vote', function(req, res, next){
 
   //Allow one vote per ip
   var voted = true;
-  votes.find({ "ip" : req.ip, "poll_id" : req.body.poll_id }, {}, function (e,result) {
+  votes.find({ 'ip' : req.ip, 'poll_id' : req.body.poll_id }, {}, function (e,result) {
     
     if (result.length  == 0){
       //never voted before
@@ -20,22 +20,22 @@ router.post('/vote', function(req, res, next){
 
       //tally vote for the poll
       polls.update(
-        { "_id" : ObjectId(req.body.poll_id) , "answers.answer" : req.body.vote},
-        { $inc : { "answers.$.vote" : 1 } }, 
+        { '_id' : ObjectId(req.body.poll_id) , 'answers.answer' : req.body.vote},
+        { $inc : { 'answers.$.vote' : 1 } }, 
         { upsert : true }    
       );
 
       //create vote for ip
       votes.insert({
-        "date" : new Date(),
-        "poll_id" : req.body.poll_id,
-        "answer" : req.body.vote,
-        "ip" : req.ip
+        'date' : new Date(),
+        'poll_id' : req.body.poll_id,
+        'answer' : req.body.vote,
+        'ip' : req.ip
       });
       
     }
     //send back poll stats
-    polls.find({ "_id" : ObjectId(req.body.poll_id) }, {}, function(err, docs){
+    polls.find({ '_id' : ObjectId(req.body.poll_id) }, {}, function(err, docs){
       if (err !== null){
         next(err);
       } else {
@@ -50,4 +50,49 @@ router.post('/vote', function(req, res, next){
 
 });
 
+/* Create a new poll
+ *
+ */
+router.post('/create', function(req, res, next){
+  var db = req.db;
+  var polls = db.get('pollcollection');
+
+  console.log(req.body);
+  var answer1 = req.body.answer1;
+  var answer2 = req.body.answer2;
+
+  //Form validation
+  var emptyMsg = 'Both options required';
+  var charMsg = '10 to 140 characters required';
+
+  req.checkBody('answer1', emptyMsg).notEmpty();
+  req.checkBody('answer1', charMsg).len(10,140);
+  req.checkBody('answer2', emptyMsg).notEmpty();
+  req.checkBody('answer2', charMsg).len(10,140);
+  
+  var errors = req.validationErrors();
+  if (errors){
+    res.send({error: true, errors: errors});
+  } else {
+    //create new poll
+    var newPoll = {
+      creationDate : new Date(),
+      answers : [
+        { answer : answer1, vote : 0 },
+        { answer : answer2, vote : 0 }
+      ],
+      createdBy : 'anonymous', //TODO add users
+    }
+
+    polls.insert(newPoll);
+    res.send({error:false});
+  }
+});
+
+/* Pull up next poll (random)
+ *
+ */
+router.post('/poll', function(req, res, next){
+
+});
 module.exports = router;
